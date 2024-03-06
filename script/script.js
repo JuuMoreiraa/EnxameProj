15d0e5be30dbede8fcba0ef82fc88cdd992d24ac
Juliana Moreira <omoreiraju@gmail.com>
2024-03-05 22:22:13 -0300

    Dropdown funcionando

--- script/script.js
+++ script/script.js
@@ -1,3 +1,11 @@
-<script>
-  AOS.init();
-</script>
+//dropdown
+$(function () {
+    $('.navbar li').hover(
+        function () {
+            $('>ul.sub:not(:animated)', this).slideDown(500);
+        },
+        function () {
+            $('>ul.sub', this).slideUp(300);
+        }
+    );
+});
