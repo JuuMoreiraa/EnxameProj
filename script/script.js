e01951be86e1e88a07e9bdfa57d1db162c02f7a8
Juliana Moreira <omoreiraju@gmail.com>
2024-03-07 20:49:53 -0300

    services e emphasis finalizados.

--- script/script.js
+++ script/script.js
@@ -1,18 +1,27 @@
 //dropdown
-$(function () {
-    $('.navbar li').hover(
-        function () {
-            $('>ul.sub:not(:animated)', this).slideDown(500);
-        },
-        function () {
-            $('>ul.sub', this).slideUp(300);
-        }
-    );
-});
+    $(function () {
+        $('.navbar li').hover(
+            function () {
+                $('>ul.sub:not(:animated)', this).slideDown(500);
+            },
+            function () {
+                $('>ul.sub', this).slideUp(300);
+            }
+        );
+    });
 
 //section home video
-window.addEventListener("load", function(){
-    let video = window.document.querySelector("#video");
-    video.play();
-    video.loop = true;
-});
+    window.addEventListener("load", function(){
+        let video = window.document.querySelector("#video");
+        video.play();
+        video.loop = true;
+    });
+
+//carousel
+    var swiper = new Swiper(".mySwiper",{
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay:{delay:2500, disableOnInteraction: false,},
+        pagination:{el:".swiper-pagination", clickable: true,},
+        navigation:{nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev"},
+    });
